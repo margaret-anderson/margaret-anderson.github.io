036e35d6778fe5d7827343c657c230ec969516fa
Margaret Anderson <maanders@caltech.edu>
2020-08-06 16:04:29 -0500

    details background bug fix and details sections

--- js/experience.js
+++ js/experience.js
@@ -1,11 +1,22 @@
 $(function(){
 	$('#h_det').on('click', function(e){
 	e.preventDefault();
-	$('#harvard_details').toggleClass('hide')
+	$('#harvard_details').toggleClass('hide');
+	if ($('#harvard_details').hasClass('hide') && $('#caltech_details').hasClass('hide')) 
+		$('#experience').addClass('up');
+	else
+		$('#experience').removeClass('up')
 	});
 
 	$('#c_det').on('click', function(e){
 	e.preventDefault();
-	$('#caltech_details').toggleClass('hide')
+	$('#caltech_details').toggleClass('hide');
+	if ($('#harvard_details').hasClass('hide') && $('#caltech_details').hasClass('hide')) 
+		$('#experience').addClass('up');
+	else
+		$('#experience').removeClass('up')
 	});
+
+	
+	
 });
