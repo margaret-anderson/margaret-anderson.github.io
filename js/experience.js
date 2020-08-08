cda88355991dfe064f0e37e7909b90934db1615d
Margaret Anderson <maanders@caltech.edu>
2020-08-08 18:14:54 -0500

    added work experiences with images and details

--- js/experience.js
+++ js/experience.js
@@ -5,7 +5,7 @@ $(function(){
 	if ($('#harvard_details').hasClass('hide') && $('#caltech_details').hasClass('hide')) 
 		$('#experience').addClass('up');
 	else
-		$('#experience').removeClass('up')
+		$('#experience').removeClass('up');
 	});
 
 	$('#c_det').on('click', function(e){
@@ -14,8 +14,43 @@ $(function(){
 	if ($('#harvard_details').hasClass('hide') && $('#caltech_details').hasClass('hide')) 
 		$('#experience').addClass('up');
 	else
-		$('#experience').removeClass('up')
+		$('#experience').removeClass('up');
 	});
+
+	$('#work1, #work2, #work3, #work4, #work5, #work6, #work7, #work8').on('click', function(e){
+		e.preventDefault();
+		$('#slide1, #slide2, #slide3, #slide4, #slide5, #slide6, #slide7, #slide8').hide();
+		$('#work1, #work2, #work3, #work4, #work5, #work6, #work7, #work8').removeClass('active_slide');
+		$(e.target).addClass('active_slide');
+		var link_name = $(e.target).attr('id');
+		switch (link_name){
+			case 'work1':
+				$('#slide1').slideToggle(500);
+				break;
+			case 'work2':
+				$('#slide2').slideToggle(500);
+				break;
+			case 'work3':
+				$('#slide3').slideToggle(500);
+				break;
+			case 'work4':
+				$('#slide4').slideToggle(500);
+				break;
+			case 'work5':
+				$('#slide5').slideToggle(500);
+				break;
+			case 'work6':
+				$('#slide6').slideToggle(500);
+				break;
+			case 'work7':
+				$('#slide7').slideToggle(500);
+				break;
+			case 'work8':
+				$('#slide8').slideToggle(500);
+				break;
+		}
+		
+		});
 
 	
 	
